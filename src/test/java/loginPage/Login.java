f0105a8556b9f8c071961392c2fa34a61252f2a0
Sushant Mulik <sushant@cateina.com>
2022-06-02 11:30:02 +0530

    Initial Commit

---
+++ src/test/java/loginPage/Login.java
@@ -0,0 +1,30 @@
+package loginPage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+	
+
+public class Login {
+	
+	
+	public static WebDriver driver;
+	@BeforeTest
+	public void setUp() {
+		driver = new ChromeDriver();	
+	}
+	@Test
+	public void dologin() {
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("sushant@cateina.com");
+		driver.findElement(By.id("Passwd")).sendKeys("Search@123");
+	}
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
